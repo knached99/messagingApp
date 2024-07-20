6206a0079d6960389432293120cd6856362af451
knached99 <khalednached@gmail.com>
2024-07-20 05:54:02 -0400

    Fixed bugs with app and ensured app is working with newer version of Flask

--- Chat/app.py
+++ Chat/app.py
@@ -1,11 +1,13 @@
-'''
+"""
 @author: Khaled
-'''
+"""
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room
 
+# Initialize Flask app and SocketIO
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*", manage_session=False)  # `manage_session=False` is recommended for better performance
 
 @app.route('/')
 def home():
@@ -23,20 +25,21 @@ def chat():
 
 @socketio.on('send_message')
 def handle_send_message_event(data):
-    app.logger.info("{} has sent message to the room {}: {}".format(data['username'], data['room'], data['message']))
+    app.logger.info(f"{data['username']} has sent message to the room {data['room']}: {data['message']}")
     socketio.emit('receive_message', data, room=data['room'])
 
 @socketio.on('join_room')
 def handle_join_room_event(data):
-    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
+    app.logger.info(f"{data['username']} has joined the room {data['room']}")
     join_room(data['room'])
     socketio.emit('join_room_announcement', data, room=data['room'])
 
 @socketio.on('leave_room')
 def handle_leave_room_event(data):
-    app.logger.info("{} has left the room {}".format(data['username'], data['room']))
+    app.logger.info(f"{data['username']} has left the room {data['room']}")
     leave_room(data['room'])
     socketio.emit('leave_room_announcement', data, room=data['room'])
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    # Ensure you use `host='0.0.0.0'` for external access or leave it as default for local access
+    socketio.run(app, debug=True, host='0.0.0.0')
